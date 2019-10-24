bb42264dd2233de55d3f2f780aeb078e364c7c69
Rob Marty <ramarty@email.wm.edu>
2019-10-24 09:33:17 -0400

    init

---
+++ Code/_industry_ntl_master.R
@@ -0,0 +1,28 @@
+# Industry and Nighttime Lights
+# Master Script
+
+# Filepaths --------------------------------------------------------------------
+if(Sys.info()[["user"]] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/Methods Papers/Industry and Nighttime lights Project"
+if(Sys.info()[["user"]] == "robmarty") project_file_path <- "~/Dropbox/World Bank/Methods Papers/Industry and Nighttime lights Project"
+
+raw_data_file_path <- file.path(project_file_path, "Data", "RawData")
+final_data_file_path <- file.path(project_file_path, "Data", "FinalData")
+figures_file_path <- file.path(project_file_path, "Results", "Figures")
+tables_file_path <- file.path(project_file_path, "Results", "Tables")
+code_file_path <- file.path(project_file_path, "Code")
+
+# Libraries --------------------------------------------------------------------
+library(raster)
+library(dplyr)
+library(parallel)
+library(pbmcapply)
+library(rgdal)
+library(rgeos)
+library(geosphere)
+library(sf)
+library(ggplot2)
+library(data.table)
+library(reshape)
+library(doBy)
+library(readstata13)
+library(haven)
